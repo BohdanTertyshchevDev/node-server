8572e1a48f7fa1f7e89fa2815be504d3fce81945
Bohdan Tertyshchev <bobterya@gmail.com>
2023-02-20 17:25:04 +0200

    server send html

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const http = require('http');
+const fs = require('fs/promises');
 
 const PORT = 3000;
 
 const requireListener = (req, res) => {
-    // console.log("REQUIRE IS HERE")
-    // const {url, method} = req;
-    // console.log(url, method);
-    res.statusCode = 418;
+    const {url} = req;
+
+    if(url === './index.html') {
+        fs.readFile('./views/index.html', 'utf8')
+        .then((data) => {
+            res.end(data)
+        })
+    } else {
+        res.statusCode = 404;
+        fs.readFile('./views/error.html', 'utf8')
+        .then((data) => {
+            res.end(data)
+        })
+    }
+
+    res.statusCode = 404;
     res.end("Hello from server");
 }
 
