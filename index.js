93a9c2659d3363c4d31999fd24c2ccfdaec25636
Bohdan Tertyshchev <bobterya@gmail.com>
2023-02-20 17:07:23 +0200

    server response

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const PORT = 3000;
 
 const requireListener = (req, res) => {
     // console.log("REQUIRE IS HERE")
-    const {url, method} = req;
-    console.log(url, method);
+    // const {url, method} = req;
+    // console.log(url, method);
+    res.statusCode = 418;
+    res.end("Hello from server");
 }
 
 const server = http.createServer(requireListener);
